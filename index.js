f772621c193d3f032b58e347956850f01c2f79cc
masoomahasnain <masooma.punjwani@student.moringaschool.com>
2022-08-21 22:45:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaeCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer(){
+    return customerName;
+}
+var bestCustomer;
+function setBestCustomer(){
+    return bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = "ali";
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = "bilal";
+}
